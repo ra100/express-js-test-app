f8971e96a47a7393a1869b640667dbbc9571f0af
Ra100 <ra100x@gmail.com>
2016-04-27 15:09:39 +0200

    switch mongodb for mongoose

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 var app = express();
 
-MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
   if (err) {
     throw err;
   }

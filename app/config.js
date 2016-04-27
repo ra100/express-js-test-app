f6222d36542826fb3681d356b9a9d0b689149451
Ra100 <ra100x@gmail.com>
2016-04-27 15:32:25 +0200

    add config file

---
+++ app/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  db: {
+    mongodb: 'mongodb://localhost:27017/test'
+  },
+  server: {
+    port: 1337
+  },
+  file: {
+    path: '../.tmp/data.log'
+  },
+  log: {
+    name: 'log'
+  }
+}

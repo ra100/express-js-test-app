e4996f43a61ec2b313ee76cb2b40bef89c6d708f
Ra100 <ra100x@gmail.com>
2016-04-27 14:41:02 +0200

    create simple express server and connect to mongoDB

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var express = require('express');
+var app = express();
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to db');
+});
+
+app.get('/track', function (req, res) {
+  res.send('tracked');
+});
+
+app.listen(1337, function () {
+  console.log('Listening on port 1337!');
+});
